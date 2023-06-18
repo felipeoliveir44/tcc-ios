5105b768a89ef2aa0ea27890f9fc0678c2d9df3e
Jvelosoo <jveloso4669@gmail.com>
2023-06-18 17:27:23 -0300

    Alterações da página de noticias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,47 @@
-// Busca todos os elementos com a classe
-const slides = document.querySelectorAll('.slide');
-// Busca o botão de avançar
-const next = document.querySelector('#next');
-// Busca o botão de retroceder
-const prev = document.querySelector('#prev');
-// Habilita ou desabilita a transição automática entre slides
-const auto = false; // Auto scroll
-// Intervalo entre transições
-const intervalTime = 2000;
-// Tempo para exibir o slide na transição automática
-let slideInterval;
+feather.replace();
 
-const nextSlide = () => {
-    // Busca o slide com a classe atual
-    const atual = document.querySelector('.atual');
-    // Remove a classe atual do slide
-    atual.classList.remove('atual');
-    // Verifica ser existe o próximo slide
-    if (atual.nextElementSibling) {
-        // Adiciona a classe atual no próximo slide
-        atual.nextElementSibling.classList.add('atual');
-    } else {
-        // Adiciona a classe atual no primeiro slide
-        slides[0].classList.add('atual');
-    }
-    setTimeout(() => atual.classList.remove('atual'));
-};
+let slideIndex = 1;
+showSlides(slideIndex);
 
-const prevSlide = () => {
-    // Busca o slide com a classe atual
-    const atual = document.querySelector('.atual');
-    // Remove a classe atual do slide
-    atual.classList.remove('atual');
-    // Verifica ser existe o slide anterior
-    if (atual.previousElementSibling) {
-        // Adiciona a classe atual no slide anterior
-        atual.previousElementSibling.classList.add('atual');
-    } else {
-        // Adiciona a classe atual no último slide
-        slides[slides.length - 1].classList.add('atual');
-    }
-    setTimeout(() => atual.classList.remove('atual'));
-};
-
-next.addEventListener('click', (e) => {
-    nextSlide(); // Invoca a função próximo slide
-    // Executa a transição entre slides
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-});
-prev.addEventListener('click', (e) => {
-    prevSlide(); // Invoca a função slide anterior
-    // Executa a transição entre slides
-    if (auto) {
-        clearInterval(slideInterval);
-        slideInterval = setInterval(nextSlide, intervalTime);
-    }
-});
-
-if (auto) {
-    // Mostra o próximo slide automaticamente depois de um tempo
-    slideInterval = setInterval(nextSlide, intervalTime);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Chamar a função showSlides automaticamente a cada 5 segundos
+setInterval(function() {
+  plusSlides(1);
+}, 5000);
+
+/* Icon feather */
+
