363eed91b744a9754ca21f633814353000fb5031
felipeoliveir44 <felipeoliveiragg@outlook.com>
2023-07-05 15:05:15 -0300

    Adição da pag de streaming

--- assets/js/cadastroAssinaturas.js
+++ assets/js/cadastroAssinaturas.js
@@ -22,22 +22,23 @@ $('#btn-enviar').click(function () {
         localStorage.setItem("email", email);
         localStorage.setItem("senha", senha);
         $.notify("Cadastro realizado!", "success");
+        window.location.href = "streaming.html";
     } else {
         $.notify("Cadastro não realizado! Tente novamente", "error");
     }
 });
 
 $('#btn-login').click(function () {
-    let emailLogin = document.getElementById("email").value;
+    
+    let emailLogin = document.getElementById("emailLogin").value;
     let senhaLogin = document.getElementById("senhaLogin").value;
     const emailLocal = localStorage.getItem("email");
     const senhaLocal = localStorage.getItem("senha");
 
-
     if (emailLogin === emailLocal && senhaLogin === senhaLocal) {
         alert("Login bem-sucedido!");
         // Redireciona para a página restrita
-        window.location.href = "assinaturas.html";
+        window.location.href = "streaming.html";
     } else {
         alert("Nome de usuário ou senha inválidos!");
     }
